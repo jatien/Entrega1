be88367737983cb75ad82b843e688aa88b2996de
Javi Torres <javierhshshhs@gmail.com>
2023-09-19 12:38:52 +0200

    actualizacion

--- datos_pinguinos.R
+++ datos_pinguinos.R
@@ -6,3 +6,22 @@ penguins %>%
 penguins %>% 
   group_by(sex,species) %>% 
   summarize(across(where(is.numeric), mean, na.rm = TRUE))
+penguins %>% 
+  group_by(species) %>% 
+  summarize(across(where(is.numeric), mean, na.rm = TRUE))
+
+bill_no_species <- ggplot(data = penguins,
+                          aes(x = bill_length_mm,
+                              y = bill_depth_mm)) +
+  geom_point() +
+  scale_color_manual(values = c("darkorange","purple","cyan4")) +
+  labs(title = "Penguin bill dimensions (omit species)",
+       subtitle = "Palmer Station LTER",
+       x = "Bill length (mm)",
+       y = "Bill depth (mm)") +
+  theme(plot.title.position = "plot",
+        plot.caption = element_text(hjust = 0, face= "italic"),
+        plot.caption.position = "plot") +
+  geom_smooth(method = "lm", se = FALSE, color = "gray50")
+
+bill_no_species
